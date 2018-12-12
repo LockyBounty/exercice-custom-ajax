369c298fa1e87db202cd17f0efb258a7777e0620
locky <bounthienglocky@gmail.com>
2018-12-12 14:51:12 +0100

    exercice ok

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
 Excercices sur Ajax personnalisé
+
+# Node.JS API Training
+
+#### Watch manager app
+
+In order to train on the basis of Coach Anthony's course I created an API to manage the students' watch in the class.
+
+#### This API can do :
+
+* POST (Only by student name)
+1. l'API va generer un ID a l'aide du module UUID
+# Node.JS-MongoDB
+
+#### Watch manager app
+
+In order to train on the basis of Coach Anthony's course I created an API to manage the students' watch in the class.
+
+#### This API can do :
+
+* POST (Only by student name)
+1. l'API va generer un ID a l'aide du module UUID
+
+* GET (Empty to GET all Students or by ID to GET 1)
+* PUT (Only by ID)
+1. Update can change the date and the subject of the watch.
+2. When add a watc hthe API gonna automaticly push the last watch date and subject into an Array
+
+* DELETE (Only By ID)
+
+#### Link of the API :
+
+1. POST : https://watchmanager.herokuapp.com/student/
+```
+POST BODY :
+{
+"studentName" : "String"
+}
+```
+
+2. GET : https://watchmanager.herokuapp.com/student/
+2. GET PARAM : https://watchmanager.herokuapp.com/student/{{YOUR ID}}
+3. PUT : https://watchmanager.herokuapp.com/student/{{YOUR ID}}
+```
+PUT BODY : 
+{
+"studentName" : "String",
+"nextWatchDate" : "String",
+"nextWatchSubject" : "String"
+}
+```
+
+4. DELETE : https://watchmanager.herokuapp.com/student/{{YOUR ID}          (parametres)
+
+
+
+(quand on enregistre des donnees, c'est dans le body sinon c'est dans les parametres)
+(parametres = mettre dans url)
+(body = dans postman creer body>raw/json)
+
+
+
+
+
+
+
---
+++ main.js
@@ -0,0 +1,121 @@
+let getButton = document.querySelector("#getButton");
+let postButton = document.querySelector("#postButton");
+let putButton = document.querySelector("#updateButton");
+let deleteButton = document.querySelector("#deleteButton");
+let clearButton = document.querySelector("#clearButton")
+
+
+let url = "https://watchmanager.herokuapp.com/student/";
+let id = "";
+
+let displayGET = () => {
+
+    fetch(url)
+        .then(response => response.json())
+        .then(res => console.log(res))
+        .catch((error) => console.warn(error))
+
+}
+
+let usePOST = () => {
+    let getVal = document.querySelector("#idPOST").value;
+    let data = {
+        studentName:`${getVal}`
+    }
+
+    const init = {
+        method: "POST", //choisir la methode
+        body: JSON.stringify(data), //entrer la valeur qui va etre envoyee, ici, transformée en string "data"
+        cache: "default",
+        credentials: "same-origin",
+        headers: {
+            'Content-Type': 'application/json'
+        }
+
+    };
+
+    fetch(url, init)
+        .then(resp => resp.json())
+        .then(response => {
+            console.log(response)
+
+        })
+        .catch(resp => {
+            console.error(resp);
+        })
+
+}
+
+let usePUT = () => {
+    let getVal0 = document.querySelector("#idUPDATE0").value;
+    let getVal1 = document.querySelector("#idUPDATE1").value;
+    let getVal2 = document.querySelector("#idUPDATE2").value;
+
+    let data0 = {
+        id: `${getVal0}`,
+        studentName: `${getVal1}`,
+        nextWatchSubject: `${getVal2}`
+    };
+
+    const init = {
+        method: "PUT", //choisir la methode GET, POST, PUT, DELETE
+        body: JSON.stringify(data0),
+
+        cache: "default",
+        credentials: "same-origin",
+        headers: {
+            'Content-Type': 'application/json'
+        }
+
+    };
+
+    fetch(`${url}${getVal0}`, init)   //mettre l'id après l'url pour passer en parametres
+        .then(resp => resp.json())
+        .then(response => {
+            console.log(response)
+
+        })
+        .catch(resp => {
+            console.error(resp);
+        })
+
+}
+
+let useDELETE = () => {
+    let getVal = document.querySelector("#idDELETE").value;
+    let data = {
+        id: `${getVal}`
+    }
+
+    const init = {
+        method: "DELETE"
+         //choisir la methode
+         //entrer la valeur qui va etre envoyee, ici, transformée en string "data"
+
+
+    };
+
+    fetch(`${url}${getVal}`, init)  //delete a mettre dans les parametres
+        .then((resp) => resp.json())
+        .then((response) => {
+            console.log(response)
+
+        })
+        .catch((resp) => {
+            console.error(resp);
+        })
+
+}
+
+let blankAll = () => {
+    let a = document.querySelectorAll("input");
+    a.value = " ok";
+    console.log("ok");
+}
+
+
+getButton.addEventListener("click", displayGET);
+postButton.addEventListener("click", usePOST);
+putButton.addEventListener("click", usePUT);
+deleteButton.addEventListener("click", useDELETE);
+clearButton.addEventListener("click", blankAll);
